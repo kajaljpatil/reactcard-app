ee3800b1225a9ab80b450cc3653a28c369f781b9
Kajal Patil <kajal.patil@ojassoft.in>
2022-08-18 19:41:05 +0530

    EditUser add

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -7,14 +7,16 @@ const EditUser = (props) => {
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("")
-    
-
+    const [company, setCompany] = useState("")
+    const [phone, setPhone] = useState("")
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then(response => {
             // console.log('res-->', response);
             setName(response.data.name);
-            setEmail(response.data.email)
+            setEmail(response.data.email);
+            setCompany(response.data.company.name);
+            setPhone(response.data.phone) 
         })
     }, [id]);
 
@@ -25,7 +27,9 @@ const EditUser = (props) => {
             `https://jsonplaceholder.typicode.com/users/${id}`,
             {
                 name: name,
-                email: email
+                email: email,
+                company:company,
+                phone:phone
             }
         ).then(response => {
             // update users state variable
@@ -45,6 +49,14 @@ const EditUser = (props) => {
                 <div className='input-group'>
                     <label>Email</label>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className='input-group'>
+                    <label>Company</label>
+                    <input type="text" value={company} onChange={(e) => setCompany(e.target.value)} />
+                </div>
+                <div className='input-group'>
+                    <label>phone</label>
+                    <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} />
                 </div>
                 <button>Save</button>
             </form>
